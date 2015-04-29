1cbb5be05387c03833d6117277db13ca74648304
aedwards92 <ancedwar@ucsc.edu>
2015-04-28 17:38:08 -0700

    updated err msgs

--- util/errcodes.cc
+++ util/errcodes.cc
@@ -19,16 +19,21 @@ namespace err
             case FILE_HANDLE_NOT_INITIALIZED:           return "FILE_HANDLE_NOT_INITIALIZED";
             case FILE_HANDLE_UNKNOWN:                   return "FILE_HANDLE_UNKNOWN";
             case FILE_NOT_OPENED:                       return "FILE_NOT_OPENED";
-            case RECORD_DOES_NOT_EXIST:                 return "RECORD_DOES_NOT_EXIST";
-            case RECORD_CORRUPT:                        return "RECORD_CORRUPT";
-            case RECORD_EXCEEDS_PAGE_SIZE:              return "RECORD_EXCEEDS_PAGE_SIZE";
             case HEADER_SIZE_CORRUPT:                   return "HEADER_SIZE_CORRUPT";
             case HEADER_PAGESIZE_MISMATCH:              return "HEADER_PAGESIZE_MISMATCH";
             case HEADER_VERSION_MISMATCH:               return "HEADER_VERSION_MISMATCH";
             case HEADER_FREESPACE_LISTS_MISMATCH:       return "HEADER_FREESPACE_LISTS_MISMATCH";
-            case HEADER_FREESPACE_LISTS_CORRUPT:         return "HEADER_FREESPACE_LIST_CORRUPT";
+            case HEADER_FREESPACE_LISTS_CORRUPT:        return "HEADER_FREESPACE_LIST_CORRUPT";
             case HEADER_SIZE_TOO_LARGE:                 return "HEADER_SIZE_TOO_LARGE";
+            case RECORD_DOES_NOT_EXIST:                 return "RECORD_DOES_NOT_EXIST";
+            case RECORD_CORRUPT:                        return "RECORD_CORRUPT";
+            case RECORD_EXCEEDS_PAGE_SIZE:              return "RECORD_EXCEEDS_PAGE_SIZE";
             case RECORD_SIZE_INVALID:                   return "RECORD_SIZE_INVALID";
+            case RECORD_DELETED:                        return "RECORD_IS_DELETED";
+            case PAGE_CANNOT_BE_ORGANIZED:              return "PAGE_CANNOT_BE_ORGANIZED";
+            case TABLE_NOT_FOUND:                       return "TABLE_NOT_FOUND";
+            case TABLE_ALREADY_CREATED;                 return "TABLE_ALREADY_CREATED";
+            case TABLE_NAME_TOO_LONG:                   return "TABLE_NAME_TOO_LONG";
             case ATTRIBUTE_INVALID_TYPE:                return "ATTRIBUTE_INVALID_TYPE";
             case OUT_OF_MEMORY:                         return "OUT_OF_MEMORY";
         }
