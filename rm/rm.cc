51ea184353058b642311d0583db03e6f62f5ed4c
aedwards92 <ancedwar@ucsc.edu>
2015-04-29 18:47:39 -0700

    commented functions

--- rm/rm.cc
+++ rm/rm.cc
@@ -13,6 +13,8 @@ RelationManager* RelationManager::instance()
 
 RelationManager::RelationManager()
 {
+    // Check if catalog file exists, if not create one
+    // Open catalog file
 }
 
 RelationManager::~RelationManager()
@@ -21,51 +23,81 @@ RelationManager::~RelationManager()
 
 RC RelationManager::createTable(const string &tableName, const vector<Attribute> &attrs)
 {
+    // Lookup tableName in dictionary
+    // If it exists, return err::TABLE_NAME_EXISTS
+    //
+    // Create a RBF
+    // Update the catalog
     return -1;
 }
 
 RC RelationManager::deleteTable(const string &tableName)
 {
+    // Delete the corresponding RBF
+    // Do equality scan on catalog Table Table
     return -1;
 }
 
 RC RelationManager::getAttributes(const string &tableName, vector<Attribute> &attrs)
 {
+    // Use the catalog to find the attributes of this table
+    // Populate attrs with proper types
     return -1;
 }
 
 RC RelationManager::insertTuple(const string &tableName, const void *data, RID &rid)
 {
+    // Use catalog to get filename corresponding tableName
+    // Open filehanlde to that file
+    // Use RBFM to insert data, store RID into rid
     return -1;
 }
 
 RC RelationManager::deleteTuples(const string &tableName)
 {
+    // Use catalog to get filename
+    // Open file
+    // Call deleteRecords
     return -1;
 }
 
 RC RelationManager::deleteTuple(const string &tableName, const RID &rid)
 {
+    // Use catalog to get filename
+    // Open file
+    // Call delete record on that RID
     return -1;
 }
 
 RC RelationManager::updateTuple(const string &tableName, const void *data, const RID &rid)
 {
+    // Use catalog to get filename
+    // Open file
+    // Call update record on that RID
     return -1;
 }
 
 RC RelationManager::readTuple(const string &tableName, const RID &rid, void *data)
 {
+    // Use catalog to get filename
+    // Open file
+    // Call read record on that RID
     return -1;
 }
 
 RC RelationManager::readAttribute(const string &tableName, const RID &rid, const string &attributeName, void *data)
 {
+    // Use catalog to get filename
+    // Open file
+    // Call readAttribute on that RID
     return -1;
 }
 
 RC RelationManager::reorganizePage(const string &tableName, const unsigned pageNumber)
 {
+    // Use catalog to get filename
+    // Open file
+    // Call reorganize page
     return -1;
 }
 
@@ -76,6 +108,9 @@ RC RelationManager::scan(const string &tableName,
       const vector<string> &attributeNames,
       RM_ScanIterator &rm_ScanIterator)
 {
+    // Use catalog to get filename
+    // Open file
+    // Call call scan 
     return -1;
 }
 
